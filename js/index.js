f1fe288a03ea90700c68a4f0bd04a94e0a344c1b
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-02-23 21:10:32 +0100

    refactor in progress

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let weather_params = {
+/*let weather_params = {
     api_key: 'bb92bfa8ce1d036519e7acaa111b6d76',
     fetchWeather: function(city) {
         fetch(
@@ -41,3 +41,37 @@ document.querySelector('.search-bar').addEventListener('keyup', function(event) 
         weather_params.search();
     }
 })
+*/
+
+const api_key = 'bb92bfa8ce1d036519e7acaa111b6d76';
+const request_button = document.querySelector('.search button');
+const weather = document.querySelectorAll('div');
+
+
+request_button.addEventListener('click', () => {
+    const query = document.querySelector('.search-bar').value;
+    if (query != '') {
+        fetchWeatherInfo(query);
+    } else {
+        alert('Please enter a valid city name!!!');
+        return;
+    }
+
+});
+
+function fetchWeatherInfo(city) {
+    console.log(city);
+    var response = fetch(
+        'http://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&units=metric&appid=' +
+        api_key
+    ).then((response) => response.json()).then((data) =>
+        data
+    );
+}
+
+document.querySelector('.button').addEventListener("click", () => {
+    document.querySelector('.search-bar').value = '';
+    weather[1].style.visibility = 'hidden';
+});
